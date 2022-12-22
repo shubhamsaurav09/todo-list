757a867cdd3c6d535efa1719c5dcc7d7c4ba4045
shubhamsaurav09 <thebeststrikertorres@gmail.com>
2022-12-22 10:10:31 +0530

    add-structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import cross from "./assets/icon-check.svg";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState();
+  const [list, setList] = useState([]);
+
+  const addTodo = () => {
+    setList([...list, { description: input, completed: false }]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="header">
+        <div className="todo-logo">TODO</div>
+      </section>
+      <section className="create-task">
+        <div className="addTask">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Create a new todo"
+          ></input>
+        </div>
+        <div className="add-button">
+          <button onClick={addTodo}>+</button>
+        </div>
+      </section>
+      <section className="tasks">
+        {list.map((todo) => (
+          <div className="todo-list">{todo.description}</div>
+        ))}
+      </section>
     </div>
   );
 }
